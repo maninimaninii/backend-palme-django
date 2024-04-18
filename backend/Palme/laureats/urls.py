dcac367f4afeb6aea83dc086bf2f33be47f3ba86
maninimaninii <153753176+maninimaninii@users.noreply.github.com>
2024-04-18 07:43:48 +0200

    ajout de la recuperation de films et ameliorations des suppressions

--- backend/Palme/laureats/urls.py
+++ backend/Palme/laureats/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-      path('', views.liste_laureats, name='liste_laureats'),
-      path('supprimer/<int:laureat_id>/', views.supprimer_laureat, name='supprimer_laureat'),
+      path('', views.accueil, name='acceuil'),
+      path('laureats/', views.liste_laureats, name='liste_laureats'),
+      path('supprimerl/<int:laureat_id>/', views.supprimer_laureat, name='supprimer_laureat'),
+      path('films/', views.liste_films, name='liste_films'),
+      path('supprimerf/<int:film_id>/', views.supprimer_film, name='supprimer_film'),
 ]
--- backend/Palme/laureats/views.py
+++ backend/Palme/laureats/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import redirect, render
-from .models import P09Laureat, P09Recompenserlaureat, P09Prix
+from .models import P09Laureat, P09Recompenserlaureat, P09Prix, P09Film, P09Recompenserfilm
 
 def liste_laureats(request):
     laureats = P09Laureat.objects.all()
     laureats_list = []
     for laureat in laureats:
-        recompenses = P09Recompenserlaureat.objects.filter(idlaureat=laureat.idlaureat)
+        recompenses = P09Recompenserlaureat.objects.filter(idlaureat=laureat.idlaureat) #les recompenes associées au lauréat (possiblement plusieurs par laureat)
         for recompense in recompenses:
-            prix = P09Prix.objects.get(idprix=recompense.idprix)
+            prix = P09Prix.objects.get(idprix=recompense.idprix) #pour chaque recompense les infos du prix concerné
             laureats_list.append({
                 "idLaureat" : laureat.idlaureat,
                 "NomLaureat": laureat.nomlaureat,
@@ -25,3 +25,41 @@ def supprimer_laureat(request, laureat_id):
     laureat = P09Laureat.objects.get(idlaureat=laureat_id)
     laureat.delete()
     return redirect('liste_laureats')
+
+
+
+
+
+
+def supprimer_film(request, film_id):
+    film = P09Film.objects.get(idfilm=film_id)
+    film.delete()
+    return redirect('liste_films')
+
+
+
+
+def accueil(request):
+    return render(request, 'laureats/accueil.html')
+
+
+
+def liste_films(request):
+    films = P09Film.objects.all()
+    films_list = []
+    for film in films : 
+        recompenses = P09Recompenserfilm.objects.filter(idfilm = film.idfilm)
+        for recompense in recompenses : 
+            prix = P09Prix.objects.get(idprix = recompense.idprix) 
+            real = P09Laureat.objects.get(idlaureat = film.idrealisateur) #on recupere l'utilisateur du film
+            films_list.append({
+                "idFilm" : film.idfilm,
+                "NomFilm": film.titrefilm,
+                "NomRealisateur" : real.nomlaureat,
+                "Pays": film.paysfilm,
+                "EditionPrix": recompense.editionprixf,
+                "NomPrix": prix.nomprix
+            })
+    return render(request, 'laureats/liste_films.html', {'films': films_list})
+
+
